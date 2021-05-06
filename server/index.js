1cce6ab1aa40d8a437439949d1f2e15abedf9c73
Evan <41348743+ajaj895@users.noreply.github.com>
2021-05-06 04:59:48 -0500

    Updated Node backend api
    Updated the api for the node.js backend.

--- server/index.js
+++ server/index.js
@@ -14,15 +14,15 @@ const database = process.env.MYSQL_DB;
 const PORT = process.env.PORT || 3001; // Port for the node server
 
 //Readying the connection
-const connection = mysql.createConnection({
+const conn = mysql.createConnection({
     host: process.env.MYSQL,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DB,
 });
 
-//Making the connection
-connection.connect(function (err) {
+//Making the connection the old days
+conn.connect(function (err) {
     //Any connection error goes here.
     if(err) {
         console.log("connection error", err.stack);
@@ -33,18 +33,32 @@ connection.connect(function (err) {
     console.log(`connected to database`);
 });
 
-const sql = "SELECT * FROM fm_radio WHERE fm_callsign REGEXP '^WMGL.*';"
+
+
+
+function getAmStations(){
+    return new Promise((resolve, reject) => {
+        const sql = `SELECT * FROM am_radio;`
+        conn.query(sql, function(err, results, fields){
+            if(err) {
+                return reject(err);
+            }
+
+            return resolve(results);
+        });
+    });
+}
 
 //test the database query
-connection.query(sql, function(err, results, fields) {
+var sql = "SELECT * FROM fm_radio WHERE fm_callsign REGEXP '^WMGL.*';";
+conn.query(sql, function(err, results, fields) { //returns an array
     if(err) throw err;
 
     console.log("Found these results", results);
 });
 
 //close the connection
-connection.end();
-
+conn.end();
 // This goes before listen
 // res stands for response
 // This is a test
@@ -54,10 +68,31 @@ app.get("/api", (req, res) => {
 
 //mariadb query
 app.get("/sqltest", (req, res) => {
-    res.json({message: `Connecting to database ${database} on ${host}`});
+    res.json({message: `Connected to database ${database} on ${host}`});
+});
+
+app.get("/sam", (req, res, next) => {
+    sql = "SELECT * FROM am_radio;";
+    /*
+    connection.query(sql, function(err, results, fields) {
+        if(err) throw err;
+    
+        console.log("Found these results", results);
+    });
+    */
+
+    return getAmStations().then((data) =>{
+        //Get the data returning a promise,
+        return res.render('AmStations', data);
+    });
 });
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
+
+//export the functions with module.export
+module.exports = {
+    getAmStations,
+}
 
